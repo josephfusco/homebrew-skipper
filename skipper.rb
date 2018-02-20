e8f12efb8f032f8067e6ed83f1297fcda176d22e
Joseph Fusco <josephfusco@users.noreply.github.com>
2018-02-20 10:29:57 -0500

    Update skipper.rb

--- skipper.rb
+++ skipper.rb
@@ -1,6 +1,6 @@
 class Skipper < Formula
   homepage "https://github.com/josephfusco/skipper"
-  url "https://github.com/josephfusco/skipper/archive/v1.0.tar.gz"
+  url "https://github.com/josephfusco/skipper/archive/v0.1.1.tar.gz"
   sha256 "c9995eb50b486359d1c0e7d2392a66e2db107e0e23224e7e9b32a53e571db7e9"
 
   def install
